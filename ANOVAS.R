ef5e4fed68e8a6f7bb0371a0d8366ed4ea1cb81a
Claudio Zanettini <claudio.zanettini@gmail.com>
2018-02-19 19:18:03 -0500

    function for anova
    Former-commit-id: af7aa1fefbd4be3aa059a02f2b24f71a66da708f

--- ANOVAS.R
+++ ANOVAS.R
@@ -2,15 +2,47 @@ install.packages("multcomp")
 install.packages("nmle")
 
 prism <- forprisms %>% 
-  dplyr::filter(channel == "EEG_FRONT" & intervals_sec %in% int & Bands == "Beta")
+  dplyr::filter(channel == "EEG_FRONT" & intervals_sec %in% int)
 
 
 prismg <- forprismg %>% 
-  dplyr::filter(channel == "EEG_FRONT" & intervals_sec %in% int & Bands == "Delta")
+  dplyr::filter(channel == "EEG_FRONT" & intervals_sec %in% int)
 
+prism$drug_dose <- as.factor(prism$drug_dose)
 
 library(multcomp)
-library(nmle)
+library(nlme)
+
+
+analisi <- function (x) {
+  lme(PSD_perc ~ drug_dose, data=x, random = ~1|subject)
+}
+
+repmesure<- by(prism, prism$Bands, analisi)
+
+anova_output <- map(repmesure, anova)
+
+
+
+calc_posthocs <- function (x) {
+  
+  posthocs <- summary(glht(x, linfct=mcp(drug_dose = "Tukey")))
+  
+  posthocs <- as.data.frame(posthocs$test[3:6])
+  posthocs["sign"] <- "no"
+  posthocs$sign[which(posthocs$pvalues <= 0.05)] <- "yes"
+  
+  to_extr <- str_detect(row.names(posthocs), "[b]")
+  
+  basel_posthoc <-posthocs[to_extr,]
+  
+  basel_posthoc
+  
+}
+
+
+map(repmesure, calc_posthocs)
+
 
 
 prism$drug_dose <- as.factor(prism$drug_dose)
